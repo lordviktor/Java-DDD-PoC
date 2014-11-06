5144d18949c087294cb50c14a31f8be84bd601ee
Victor Silva <victor.silva@mobicare.com.br>
2014-11-06 20:51:29 -0200

    Configurando dbunit

--- src/test/java/br/com/victor/JavaDddExample/MedicamentoControllerTest.java
+++ src/test/java/br/com/victor/JavaDddExample/MedicamentoControllerTest.java
@@ -7,16 +7,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring/application-context.xml", "classpath:/spring/persistence-context.xml",
         "classpath:/spring/apiv1-servlet.xml"})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class})
 @WebAppConfiguration
 public class MedicamentoControllerTest {
 
@@ -31,7 +41,8 @@ public class MedicamentoControllerTest {
     }
 
     @Test
+    @DatabaseSetup(type = DatabaseOperation.CLEAN_INSERT, value = "/datasets/myDataset.xml")
     public void tryToRetrieveAllMedicaments() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/healthCheck")).andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/farmacia/1/medicamento")).andExpect(status().isOk());
     }
 }
