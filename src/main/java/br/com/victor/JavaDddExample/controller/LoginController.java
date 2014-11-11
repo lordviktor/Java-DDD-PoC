5c587af6e0eec802f9c730b2c54db4778ece8d91
lordviktor <victorjosesilvapinto@hotmail.com>
2014-11-11 00:11:43 -0200

    Adicionei request mapping faltante

--- src/main/java/br/com/victor/JavaDddExample/controller/LoginController.java
+++ src/main/java/br/com/victor/JavaDddExample/controller/LoginController.java
@@ -2,9 +2,12 @@ package br.com.victor.JavaDddExample.controller;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import br.com.victor.JavaDddExample.domain.Funcionario;
 import br.com.victor.JavaDddExample.exception.InvalidArgumentException;
@@ -19,6 +22,8 @@ public class LoginController {
 	@Autowired
 	private FuncionarioRepository funcionarioRepository;
 
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
 	public void login(@RequestBody Login loginData) {
 		if (loginData == null) {
 			throw new InvalidArgumentException();
